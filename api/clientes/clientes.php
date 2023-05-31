c7b55e73ac7d256fec75cbe6c9e0d68374a61575
Lucas Gabriel <pintolucas996@gmail.com>
2023-05-30 23:40:00 -0300

    Método POST

--- api/clientes/clientes.php
+++ api/clientes/clientes.php
@@ -3,41 +3,13 @@
 // var_dump("teste");
 
 if ($api = 'clientes') {
+
     if ($method == "GET") {
+        include_once "get.php";       
+    }
 
-        // var_dump($acao);
-        if ($acao === '' && $param === '') {
-            echo json_encode(['ERRO' => 'Caminho não encontrado!']);
-        }
-        if ($acao == 'lista' && $param == '') {
-             // var_dump("estamos no método get");
-            $db = DB::connect();
-            $rs = $db->prepare("SELECT * FROM clientes ORDER BY nome");
-            $rs->execute();
-            $obj = $rs->fetchAll(PDO::FETCH_ASSOC);
-       
-            // print_r($obj);
-            if ($obj) {
-                echo json_encode(["dados" => $obj]);
-            } else {
-                echo json_encode(["dados" => 'Não existem dados para retornar']);
-            }
-        }
-
-        if ($acao == 'lista' && $param != '') {
-            // var_dump("estamos no método get");
-           $db = DB::connect();
-           $rs = $db->prepare("SELECT * FROM clientes WHERE id={$param}");
-           $rs->execute();
-           $obj = $rs->fetchObject();
-      
-           // print_r($obj);
-           if ($obj) {
-               echo json_encode(["dados" => $obj]);
-           } else {
-               echo json_encode(["dados" => 'Não existem dados para retornar']);
-           }
-       }
+    if ($method == "POST") {
+        include_once "post.php";       
     }
 
 }
